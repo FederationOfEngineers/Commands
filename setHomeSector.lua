512476744fbd1b6d8a08a3f8c2fed28521d96a28
Joshua Homer <countjoshington@gmail.com>
2017-01-26 20:36:36 -0700

    fixed up the command script and added a few validations.

--- setHomeSector.lua
+++ setHomeSector.lua
@@ -1,26 +1,33 @@
+--- Place this file in the scripts/commands folder
 package.path = package.path .. ";data/scripts/lib/?.lua"
 
 function execute(sender, commandName, ...)
     local args = {...}
 
-    local str = ""
-    local x = args[1]
-    local y = args[2]
+    local x = tonumber(args[1])
+    local y = tonumber(args[2])
 
     if x ~= nil and y ~= nil then
-    	Server():broadcastChatMessage("", 0, string.format("Setting home sector to sector (%s, %s)!", x, y))
+	    if (x > 500 or x < -500) or (y > 500 or y < -500) then
+	    	Server():broadcastChatMessage("", 2, "The coordinate values must be between -500 and 500")
+
+	        return 0, "", ""
+	    end
+
+    	Server():broadcastChatMessage("", 1, string.format("Setting home sector to sector (%s, %s)!", x, y))
     	Player():setHomeSectorCoordinates(x, y)
-    else
-    	Server():broadcastChatMessage("", 0, "Please provide the x and y coordinates for the sector you'd like to set as home.")
+        return 0, "", ""
     end
+
+    Server():broadcastChatMessage("", 2, "The coordinate values provided are invalid")
 
     return 0, "", ""
 end
 
 function getDescription()
-    return "Set your home sector to a specific sector or the current sector"
+    return "Set your home sector to a specific sector"
 end
 
 function getHelp()
-    return "Set your home sector to the sector given. If nothing is passed into the command when called it will set your home sector to your current sector. Usage: /setHomeSector [x y]"
+    return "Set your home sector to the sector given. Restrictions: x and y values must be between -500 and 500. Usage: /setHomeSector -10 -234"
 end
